0b66957484141fc92ffde35959a9403ecf88c148
Srahul2244 <99595875+Srahul2244@users.noreply.github.com>
2022-02-25 14:01:26 +0530

    seephora
    hair added

---
+++ brands/hair.js
@@ -0,0 +1,74 @@
+var hairMask =[
+    
+        {
+            image:"https://www.sephora.com/productimages/sku/s2035079-main-zoom.jpg?imwidth=630",
+            name:"amika",
+            name2:" Soulfood Nourishing Hair Mask",
+            price:30,
+            about:"An ultra-rich, hydrating hair mask that is packed with vitamins and nutrients to moisturize, soften, and smooth.",
+         },
+         {
+            image:"https://www.sephora.com/productimages/sku/s2441095-main-zoom.jpg?imwidth=630",
+            name:"Olaplex",
+            name2:"No. 8 Bond Intense Moisture Hair Mask",
+            price:39,
+            about:"Infused with patented OLAPLEX Bond Building technology, this highly concentrated reparative mask adds shine, smoothness and body while providing intense moisture to treat damaged hair.",
+         },
+         {
+            image:"https://www.sephora.com/productimages/sku/s1784636-main-zoom.jpg?imwidth=630",
+            name:"Briogeo",
+            name2:"  Don't Despair, Repair!™ Deep Conditioning Hair Mask",
+            price:39,
+            about:"A weekly, clinically tested mask that strengthens damaged hair and helps prevent future damage. ",
+         },
+         {
+            image:"https://www.sephora.com/productimages/sku/s2538908-main-zoom.jpg?imwidth=270&pb=clean-planet-positive-badge-2021&imwidth=328",
+            name:"Briogeo",
+            name2:"     Don't Despair Repair Rice Water Protein + Moisture Strengthening Treatment",
+            price:39,
+            about:"An intensive weekly treatment powered by rice water that delivers a balance of protein and moisture and is scientifically proven to double the strength of damaged, brittle, and broken hair after one use.",
+         },
+         {
+            image:"https://www.sephora.com/productimages/sku/s2497790-main-zoom.jpg?imwidth=270&imwidth=328",
+            name:"amika",
+            name2:" The Kure Intense Bond Repair Hair Mask",
+            price:39,
+            about:" A reparative, ultra-moisturizing hair mask that is clinically proven to significantly hydrate, repair, strengthen, and improve manageability.",
+         }, 
+         {
+            image:"https://www.sephora.com/productimages/sku/s2413896-main-zoom.jpg?imwidth=270&imwidth=328",
+            name:"OUAI",
+            name2:" Treatment Mask for Fine and Medium Hair",
+            price:39,
+            about:"A mask that helps restore hydration, repair damage, and add shine to fine/medium hair with shea butter, panthenol, and hydrolyzed keratin.",
+         }, 
+         {
+            image:"https://www.sephora.com/productimages/sku/s2547248-main-zoom.jpg?imwidth=270&pb=2020-03-sephora-clean-2019&imwidth=328",
+            name:"K18 Biomimetic Hairscience",
+            name2:"  Leave-in Molecular Repair Hair Mask ",
+            price:39,
+            about:"A leave-in treatment mask for all hair types that clinically reverses damage in four minutes. The patented peptide technology works to repair damage from bleach, color, chemical services, and heat, restoring strength, softness, smoothness, and bounce to hair.",
+         },
+         {
+            image:"https://www.sephora.com/productimages/sku/s2318582-main-zoom.jpg?imwidth=270&pb=allure-best-2020&imwidth=328",
+            name:"Briogeo",
+            name2:"Be Gentle, Be Kind™ Avocado + Kiwi Mega Moisture Superfoods Hair Mask ",
+            price:39,
+            about:"A deeply nourishing, protein-free hair mask with vitamins, minerals, and phytonutrients clinically shown to boost moisture for more hydrated hair.",
+         }, 
+         {
+            image:"https://www.sephora.com/productimages/sku/s1999622-main-zoom.jpg?imwidth=270&imwidth=328",
+            name:"Moroccanoil",
+            name2:" Intense Hydrating Mask",
+            price:39,
+            about:"A high-performance, argan-oil-rich mask formulated for medium to thick dry hair that improves hydration, texture, elasticity, shine, and manageability.",
+         }, 
+         {
+            image:"https://www.sephora.com/productimages/sku/s2413904-main-zoom.jpg?imwidth=270&imwidth=328",
+            name:"OUAI",
+            name2:"Treatment Mask for Thick Hair ",
+            price:39,
+            about:"A mask that restores hydration, repairs damage, and adds shine to thick hair with almond, macadamia, and olive oils, shea and illipe butters, and hydrolyzed keratin.",
+         },    
+]
+localStorage.setItem("hairData",JSON.stringify(hairMask))
---
+++ brands/hair2.js
@@ -0,0 +1,74 @@
+var hairOil =[
+    {
+        image:"https://www.sephora.com/productimages/sku/s2266765-main-zoom.jpg?imwidth=630",
+        name:"Olaplex",
+        name2:" No. 7 Bonding Hair Oil ",
+        price:39,
+        about:"An ultra-lightweight, reparative, styling oil that increases shine, softness, and color vibrancy, while minimizing flyaways and protecting hair against heat damage.",
+     },
+     {
+        image:"https://www.sephora.com/productimages/sku/s2030286-main-zoom.jpg?imwidth=630",
+        name:"Moroccanoil",
+        name2:"  Moroccanoil Treatment ",
+        price:39,
+        about:"A versatile, argan oil-infused hair treatment and styler that promotes softer, stronger hair with increased shine and less frizz.",
+     },
+     {
+        image:"https://www.sephora.com/productimages/sku/s2380285-main-zoom.jpg?imwidth=630",
+        name:"Gisou",
+        name2:" Mini Honey Infused Hair Oil ",
+        price:39,
+        about:"An iconic, travel-friendly hair oil—based on a family recipe—that is formulated to rebuild and repair your hair with the power of Mirsalehi honey.",
+     },
+     {
+        image:"https://www.sephora.com/productimages/sku/s1728351-main-zoom.jpg?imwidth=630",
+        name:"Verb",
+        name2:"  Ghost Weightless Hair Oil ",
+        price:39,
+        about:"A weightless, restorative oil that hydrates, strengthens, and smooths strands for shiny, frizz-free hair.",
+     },
+     {
+        image:"https://www.sephora.com/productimages/sku/s2126985-main-zoom.jpg?imwidth=630",
+        name:"Kérastase",
+        name2:"Elixir Ultime Hydrating Hair Oil Serum ",
+        price:39,
+        about:"A blend of four precious hair oils that adds hydration and prevents breakage for anti-frizz shine.",
+     },
+     {
+        image:"https://www.sephora.com/productimages/sku/s1802099-main-zoom.jpg?imwidth=630",
+        name:"OUAI",
+        name2:"Hair Oil",
+        price:39,
+        about:"A lightweight, multitasking oil that fights frizz, prevents heat/UV damage, and delivers a high-gloss, super-smooth finish to all hair types.",
+     },
+     {
+        image:"https://www.sephora.com/productimages/sku/s2535292-main-zoom.jpg?imwidth=630",
+        name:"Sol de Janeiro",
+        name2:" Brazilian Glossy™ Nourishing Anti-Frizz Hair Oil",
+        price:39,
+        about:"A nourishing oil that fights frizz, adds shine, protects against heat, and locks in your style while moisturizing and repairing dry, damaged hair.",
+     },
+     {
+        image:"https://www.sephora.com/productimages/sku/s2496974-main-zoom.jpg?imwidth=630",
+        name:"JVN",
+        name2:" Complete Pre-Wash Scalp & Hair Treatment Oil",
+        price:39,
+        about:"A lightweight nutrient-dense pre-wash treatment oil that clarifies and soothes the scalp for stronger, shinier, longer, healthier-looking hair.",
+     },
+     {
+        image:"https://www.sephora.com/productimages/sku/s2438737-main-zoom.jpg?imwidth=270&pb=2020-03-allure-best-2019&imwidth=328",
+        name:"Oribe",
+        name2:"   Gold Lust Nourishing Hair Oil ",
+        price:39,
+        about:"A luxuriously light, multiuse oil that absorbs quickly, adding shine, hydration, and silkiness to hair without weighing it down.",
+     },
+     {
+        image:"https://www.sephora.com/productimages/sku/s2540060-main-zoom.jpg?imwidth=270&pb=clean-planet-positive-badge-2021&imwidth=328",
+        name:"JVN",
+        name2:"Complete Nourishing Hair Oil Shine Drops ",
+        price:39,
+        about:"A silicone-free, weightless, multitasking hair oil that reduces frizz, hydrates, protects against damage, and quickly leaves a high-shine finish.",
+     },
+     
+]
+localStorage.setItem("hairObj" ,JSON.stringify(hairOil))
